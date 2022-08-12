f21faeeadf328a0f9e142b606c724b46f55d2b9a
Qori El-Hafizh <contact@qori.dev>
2022-08-12 14:53:07 +0700

    seed

---
+++ lib/hive_init.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'data/adapters/duration_adapter.dart';
+import 'data/adapters/color_adapter.dart';
+import 'data/models/loop_model.dart';
+import 'data/models/preset_model.dart';
+import 'data/models/task_model.dart';
+
+Future<void> initializeHive() async {
+  Hive.registerAdapter(DurationAdapter());
+  Hive.registerAdapter(ColorAdapter());
+  Hive.registerAdapter(TaskModelAdapter());
+  Hive.registerAdapter(LoopModelAdapter());
+  Hive.registerAdapter(PresetModelAdapter());
+  await Hive.initFlutter();
+  final presetBox = await Hive.openBox<PresetModel>('presets');
+  final prefsBox = await Hive.openBox('prefs');
+
+  if (!prefsBox.get("app_ran", defaultValue: false)) {
+    presetBox.add(
+      PresetModel(
+        name: "Pomodoro",
+        loops: [
+          LoopModel(
+            tasks: [
+              TaskModel(
+                name: "Work",
+                duration: const Duration(minutes: 25),
+                color: Colors.red,
+              ),
+              TaskModel(
+                name: "Rest",
+                duration: const Duration(minutes: 5),
+                color: Colors.green,
+              ),
+            ],
+            sets: 4,
+          ),
+          LoopModel(
+            tasks: [
+              TaskModel(
+                name: "Work",
+                duration: const Duration(minutes: 25),
+                color: Colors.red,
+              ),
+              TaskModel(
+                name: "Long Rest",
+                duration: const Duration(minutes: 15),
+                color: Colors.green,
+              ),
+            ],
+            sets: 1,
+          ),
+        ],
+      ),
+    );
+    prefsBox.put("app_ran", true);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:interval/data/adapters/duration_adapter.dart';
 import 'package:interval/di.dart';
-
+import 'package:interval/hive_init.dart';
 import 'app/app.dart';
-import 'data/adapters/color_adapter.dart';
-import 'data/models/loop_model.dart';
-import 'data/models/preset_model.dart';
-import 'data/models/task_model.dart';
 
 void main() async {
-  Hive.registerAdapter(DurationAdapter());
-  Hive.registerAdapter(ColorAdapter());
-  Hive.registerAdapter(TaskModelAdapter());
-  Hive.registerAdapter(LoopModelAdapter());
-  Hive.registerAdapter(PresetModelAdapter());
-  await Hive.initFlutter();
-  await Hive.openBox<PresetModel>('presets');
   setup();
+  await initializeHive();
   runApp(MyApp());
 }
