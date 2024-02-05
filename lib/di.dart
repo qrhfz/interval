8e639de35411dd19329d0a26a606a6860f56253b
Qori El-Hafizh <contact@qori.dev>
2024-02-05 23:51:56 +0700

    remove unused audio service file

--- lib/di.dart
+++ lib/di.dart
@@ -1,5 +1,4 @@
 import 'package:get_it/get_it.dart';
-import 'package:interval/audio.dart';
 import 'package:interval/data/preset_db.dart';
 import 'package:interval/data/preset_repo.dart';
 
@@ -8,5 +7,4 @@ final getIt = GetIt.instance;
 void setup() {
   getIt.registerSingleton<PresetDB>(PresetDB());
   getIt.registerSingleton(PresetRepo(getIt.get<PresetDB>()));
-  getIt.registerSingleton<AudioService>(AudioService());
 }
