8fc1852e524e1a6e5e0dcc6d50d35d6124362dec
Qori El-Hafizh <contact@qori.dev>
2024-02-09 09:54:05 +0700

    remove just_audio

--- android/app/src/main/kotlin/dev/qori/interval/MainActivity.kt
+++ android/app/src/main/kotlin/dev/qori/interval/MainActivity.kt
@@ -4,9 +4,10 @@ import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.WindowManager
 import androidx.lifecycle.lifecycleScope
 import io.flutter.embedding.android.FlutterActivity
@@ -19,56 +20,20 @@ import org.koin.android.ext.android.inject
 class MainActivity : FlutterActivity() {
     private val timerServiceHandler: TimerService.Handler by inject()
 
-    companion object {
-        const val FLUTTER_CHANNEL_ID = "interval.qori.dev/notification"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        makeNotificationChannel()
+        requestNotificationPermission()
     }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create the NotificationChannel.
-            val name = "timer notification"
-            val descriptionText = "to show the remaining time when doing a task"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val mChannel = NotificationChannel(TimerService.NOTIFICATION_CHANNEL_ID, name, importance)
-            mChannel.description = descriptionText
-            // Register the channel with the system. You can't change the importance
-            // or other notification behaviors after this.
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(mChannel)
-        }
-
-
-        val channel = MethodChannel(flutterEngine .dartExecutor.binaryMessenger, FLUTTER_CHANNEL_ID)
-
-        channel.setMethodCallHandler { call, result ->
-                when (call.method) {
-                    "showTimer" -> handleShowTimer(call)
-                    "stopTimer" -> handleStopTimer()
-                    "dismissTimer" -> handleDismissTimer()
-                    else -> result.notImplemented()
-                }
-            }
-
-        lifecycleScope.launch {
-            timerServiceHandler.paused.collect{
-                channel.invokeMethod("onTimerPaused", null)
-            }
-        }
-
-        lifecycleScope.launch {
-            timerServiceHandler.stopped.collect{
-                channel.invokeMethod("onTimerDismissed", null)
-            }
-        }
-
-        requestNotificationPermission()
+        setupTimerPlatformChannel(flutterEngine)
+        setupAudioPlatformChannel(flutterEngine)
     }
 
     private fun handleShowTimer(call: MethodCall) {
@@ -95,5 +60,89 @@ class MainActivity : FlutterActivity() {
             return
         }
         requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS),1)
+    }
+
+    private fun makeNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel.
+            val name = "timer notification"
+            val descriptionText = "to show the remaining time when doing a task"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val mChannel = NotificationChannel(TimerService.NOTIFICATION_CHANNEL_ID, name, importance)
+            mChannel.description = descriptionText
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+        }
+    }
+
+    private fun setupTimerPlatformChannel(flutterEngine: FlutterEngine){
+        val channel = MethodChannel(flutterEngine .dartExecutor.binaryMessenger, "interval.qori.dev/notification")
+        channel.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "showTimer" -> handleShowTimer(call)
+                "stopTimer" -> handleStopTimer()
+                "dismissTimer" -> handleDismissTimer()
+                else -> result.notImplemented()
+            }
+        }
+
+        lifecycleScope.launch {
+            timerServiceHandler.paused.collect{
+                channel.invokeMethod("onTimerPaused", null)
+            }
+        }
+
+        lifecycleScope.launch {
+            timerServiceHandler.stopped.collect{
+                channel.invokeMethod("onTimerDismissed", null)
+            }
+        }
+    }
+
+    private fun setupAudioPlatformChannel(flutterEngine: FlutterEngine){
+        val bell = audioPlayer("flutter_assets/assets/sounds/bell.ogg")
+        val bell2 = audioPlayer("flutter_assets/assets/sounds/bell2.ogg")
+
+
+        val audioFlutterChannel = MethodChannel(flutterEngine .dartExecutor.binaryMessenger, "interval.qori.dev/audio")
+        audioFlutterChannel.setMethodCallHandler { call, result ->
+            when(call.method){
+                "bell"->{
+                    bell.start()
+                }
+                "bell2"->{
+                    bell2.start()
+                }
+                "setVolume"->{
+                    val volume = call.argument<Double>("volume")?.toFloat() ?: return@setMethodCallHandler
+                    bell.setVolume(volume, volume)
+                    bell2.setVolume(volume, volume)
+                }
+                else->result.notImplemented()
+            }
+        }
+    }
+
+    private fun audioPlayer(asset:String):MediaPlayer{
+        val assetFd = assets.openFd(asset)
+        return MediaPlayer().apply {
+            if (Build.VERSION.SDK_INT >= 21) {
+                setAudioAttributes(
+                    AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .build()
+                )
+            }
+
+            if (Build.VERSION.SDK_INT >= 24){
+                setDataSource(assetFd)
+            }else{
+                setDataSource(assetFd.fileDescriptor, assetFd.startOffset, assetFd.length)
+            }
+            prepare()
+        }
     }
 }
--- lib/app/interval/timer_auido_controller.dart
+++ lib/app/interval/timer_auido_controller.dart
@@ -1,47 +1,54 @@
 import 'package:flutter/foundation.dart';
-import 'package:just_audio/just_audio.dart';
+import 'package:flutter/services.dart';
 
 class TimerAudioController {
-  final bell = AudioPlayer();
-  final bell2 = AudioPlayer();
+  final _channel = const MethodChannel('interval.qori.dev/audio');
+
+  // final bell = AudioPlayer();
+  // final bell2 = AudioPlayer();
 
   final _mute = ValueNotifier(false);
   ValueListenable<bool> get mute => _mute;
 
-  bool _isReady = false;
+  // bool _isReady = false;
 
   TimerAudioController() {
-    Future.any([
-      bell.setAsset("assets/sounds/bell.ogg"),
-      bell2.setAsset("assets/sounds/bell2.ogg")
-    ]).then((value) {
-      _isReady = true;
-    });
+    // Future.any([
+    //   bell.setAsset("assets/sounds/bell.ogg"),
+    //   bell2.setAsset("assets/sounds/bell2.ogg")
+    // ]).then((value) {
+    //   _isReady = true;
+    // });
 
     mute.addListener(() {
-      setVolume(mute.value ? 0 : 100);
+      setVolume(mute.value ? 0 : 1.0);
     });
   }
 
   void setVolume(double n) {
-    if (!_isReady) return;
+    // if (!_isReady) return;
 
-    bell.setVolume(n);
-    bell2.setVolume(n);
+    // bell.setVolume(n);
+    // bell2.setVolume(n);
+
+    _channel.invokeMethod("setVolume", {"volume": n});
   }
 
   void playSetDoneSound() {
-    if (!_isReady) return;
+    _channel.invokeMethod("bell");
+    // if (!_isReady) return;
 
-    bell.seek(Duration.zero);
-    bell.play();
+    // bell.seek(Duration.zero);
+    // bell.play();
   }
 
   void finish() {
-    if (!_isReady) return;
+    _channel.invokeMethod("bell2");
 
-    bell2.seek(Duration.zero);
-    bell2.play();
+    // if (!_isReady) return;
+
+    // bell2.seek(Duration.zero);
+    // bell2.play();
   }
 
   void muteVolume() {
